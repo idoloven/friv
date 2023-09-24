191dc91b67d10f87dde8dbe350a72cb4463f8046
Ido Loven <idoloven123@gmail.com>
2023-09-24 23:34:16 +0300

    added iframes and a div on top for closing the iframes

--- frontend/src/components/gameSquare/GameSquare.js
+++ frontend/src/components/gameSquare/GameSquare.js
@@ -1,7 +1,24 @@
 import React, { useState } from 'react';
+import CloseIcon from '@mui/icons-material/Close';
+import IconButton from '@mui/material/IconButton';
 import backgroundImage from '../../images/das.png'; // Import the image
 
 function GameSquare({label }) {
+  const [isMounted, setIsMounted] = useState(false);
+  const [iframeVisible, setIframeVisible] = useState(false);
+
+  const handleIframeLoad = () => { //waits for the iframe to load and then render the top div
+    setIsMounted(true);
+  };
+
+  const openIframe = () => { 
+    setIframeVisible(true);
+  };
+
+  const closeIframe = () => {
+    setIframeVisible(false);
+  };
+
   const handleMouseEnter = (event) => {
     event.target.style.width = '140px'; // Set the opacity on hover
     event.target.style.opacity = '0.8';
@@ -29,17 +46,59 @@ function GameSquare({label }) {
     transition: 'width 0.5s', // Add opacity transition
   }
 
+  const overlayStyle = {
+    position: 'fixed',
+    top: 0,
+    left: 0,
+    width: '100%',
+    height: '100%',
+    zIndex: 9999,
+    display: iframeVisible ? 'flex' : 'none', // Center content when visible
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+  };
 
+  const iframeStyle = {
+    border: 'none',
+    width: '90%', // Adjust the width as needed
+    height: '90vh', // Adjust the height as needed
+    borderRadius: '0px 0px 10px 10px',
+  };
+
+  
+  const frameTopStyle = {
+   width: '90%',
+   backgroundColor: 'white',
+   borderRadius: '10px 10px 0px 0px'
+  };
 
   return (
+    <>
     <button
+      onClick={openIframe}
       style={buttonStyle}
       onMouseEnter={handleMouseEnter}
       onMouseLeave={handleMouseLeave}
     >
       {label}
     </button>
+    <div style={overlayStyle}>
+        {isMounted ?
+        <div style={frameTopStyle}>
+        <IconButton onClick={closeIframe} style={{ color: 'red' }}>
+          <CloseIcon />
+        </IconButton>
+        </div> :
+       null}
+        <iframe
+          src="https://www.example.com" // Replace with the URL you want to display
+          title="My Iframe"
+          onLoad={handleIframeLoad}
+          style={iframeStyle}
+        ></iframe>
+      </div>
+    </>
   );
 }
-
 export default GameSquare;
